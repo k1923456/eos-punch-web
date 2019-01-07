9d75455dd21bcfa25555b619b2a3bf58acaa9aa3
Jacky Chen <jacky.chen@awesomegaming.tw>
2019-01-07 17:51:07 +0800

    add global active button animation

--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Home from 'scenes/Home';
 import './assets/styles/reset.css';
-
+import './assets/styles/animation.css';
 
 ReactDOM.render(
   <Home ref={ el => window.dice = el} />,
